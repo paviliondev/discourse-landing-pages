545852e61904893f4491938aa5166bf9f7247084
angusmcleod <angus@mcleod.org.au>
2021-04-14 21:00:57 +1000

    Move javascript into plugin

--- assets/javascripts/page/common.js
+++ assets/javascripts/page/common.js
@@ -50,3 +50,48 @@ var $body = $("body");
 $window.on("scroll", function() {
   $body.toggleClass('scrolled', $window.scrollTop() > 0);
 });
+
+function getElementOffset(el) {
+  var elOffset = el.offset().top;
+  var elHeight = el.height();
+  var windowHeight = $(window).height();
+  var offset;
+
+  if (elHeight < windowHeight) {
+    offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
+  } else {
+    offset = elOffset;
+  }
+
+  return offset;
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  $("a.scroll-and-center").on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: getElementOffset($(e.target.getAttribute('href')))
+    }, 400);
+  });
+
+  var $memberList = $('#member-list');
+  var $toggleList = $memberList.find('.item-list-toggle');
+  var $itemList = $memberList.find('.item-list');
+
+  function handleGroupToggle(group) {
+    if (group && group !== 'everyone') {
+      $itemList.find(`.item:not(.${group})`).removeClass("show");
+      $itemList.find(`.item.${group}`).addClass("show");
+    } else {
+      $itemList.find('.item').addClass('show');
+    }
+  }
+
+  handleGroupToggle(window.location.hash.replace(/^#/, ''));
+
+  $memberList.find('a.toggle').on("click", function(e) {
+    $toggleList.find('.toggle').removeClass('active');
+    $(e.target).addClass('active');
+    handleGroupToggle(e.target.getAttribute("data-group"));
+  });
+});
