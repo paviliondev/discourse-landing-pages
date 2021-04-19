a0b7c0f4724b87aa358b2e03f4ea7d824d08340e
angusmcleod <angus@mcleod.org.au>
2021-04-19 13:11:42 +1000

    Mock ember and i18n to allow theme settings to load

--- assets/javascripts/landing-page-assets.js
+++ assets/javascripts/landing-page-assets.js
@@ -1,3 +1,4 @@
 //= require set-prototype-polyfill
 //= require jquery
 //= require discourse-loader
+//= require discourse/app/lib/theme-settings-store
---
+++ assets/javascripts/landing-page-globals.js
@@ -0,0 +1,8 @@
+window.Ember = Object.create({
+  inject: {},
+  computed: {},
+  RSVP: {},
+  String: {},
+  run: {}
+});
+window.I18n = Object.create({});
